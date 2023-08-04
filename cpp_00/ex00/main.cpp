2f616f767065738927ffa0ca293565b5f5e05729
jweeeezy <jwillert@42heilbronn.student.de>
2023-08-04 17:56:52 +0200

    ADDED: ex00 and draft of ex01

---
+++ cpp_00/ex00/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwillert <jwillert@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 15:43:17 by jwillert          #+#    #+#             */
+/*   Updated: 2023/08/04 14:35:10 by jwillert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>	// needed for std::cout and std::endl
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for(size_t i = 1; i < argc; ++i)
+	{
+		std::string	str;
+
+		str = argv[i];
+		for (std::string::iterator str_it = str.begin();
+			str_it != str.end();
+			++str_it)
+		{
+			std::cout << (char) toupper(*str_it);
+		}
+	}
+	std::cout << std::endl;
+    return (0);
+}
+
+
+
