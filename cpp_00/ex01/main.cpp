328d1de09bc66a1dc280098fcc95a60b1fe7c582
jweeeezy <jwillert@42heilbronn.student.de>
2023-08-09 12:45:32 +0200

    ADDED: project sketch; CHANGED: debug_log

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -1,34 +1,55 @@
 
-#include <iostream>	// needed for cin, cout, cerr
-#include "phonebook.hpp"	// needed for MACROS
+#include <iostream>				// needed for cin, cout, cerr
+#include "phonebook.hpp"		// needed for MACROS
+#include "class.phonebook.hpp"	// needed for Phonebook
+
+void	debug_log(std::string message)
+{
+	if (DEBUG == true)
+		std::clog << "log: " << message << std::endl;
+	return ;
+}
+
+void	debug_log(std::string message, int exit_status)
+{
+	if (DEBUG == true)
+		std::clog << "log: " << message << exit_status << std::endl;
+	return ;
+}
 
 int	main_loop(void)
 {
-	std::string	user_input;
-
 	while (1)
 	{
+		Phonebook	pb;
+		std::string	user_input;
+
 		std::cout << MESSAGE_WELCOME << std::endl;
 		std::cin >> user_input;
-
-		if (DEBUG)
-			std::clog << "log: user inputted: [" << \
-			user_input << "]" << std::endl;
-
 		if (user_input.compare("ADD") == 0)
 		{
-			if (DEBUG)
-				std::clog << "log: ADD called" << std::endl;
+			debug_log("ADD called");
 		}
 		else if(user_input.compare("SEARCH") == 0)
 		{
-			if (DEBUG)
-				std::clog << "log: SEARCH called" << std::endl;
+			std::string	index;
+
+			debug_log("SEARCH called");
+			pb.display_all();
+			// @todo pb.display();
+			std::cout << MESSAGE_SEARCH << std::endl;
+			std::cin >> index;
+
+			// @todo uncaught exception!!
+			if (std::stoi(index) < 0 || std::stoi(index) >= 9)
+			{
+				std::cout << MESSAGE_SEARCH_BAD << std::endl;
+				// @todo reloop!
+			}
 		}
 		else if(user_input.compare("EXIT") == 0)
 		{
-			if (DEBUG)
-				std::clog << "log: EXIT called" << std::endl;
+			debug_log("EXIT called");
 			break ;
 		}
 	}
@@ -40,12 +61,9 @@ int	main(void)
 	int	exit_status;
 
 	exit_status = EXIT_SUCCESS;
-	if (DEBUG == true)
-		std::clog << "log: program started..." << std::endl;
+	debug_log("program started");
 	exit_status = main_loop();
-	if (DEBUG == true)
-		std::clog << "log: program exited with exit_status = " \
-		<< exit_status << std::endl;
+	debug_log("program exited with exit_status = ", exit_status);
 	return (exit_status);
 }
 
--- cpp_00/ex01/phonebook.hpp
+++ cpp_00/ex01/phonebook.hpp
@@ -1,15 +1,20 @@
 
-
 #ifndef PHONEBOOK_H
 # define PHONEBOOK_H
 
-# define MESSAGE_WELCOME "Please enter one of the following commands:" \
-						 " ADD, SEARCH, EXIT"
-//# define MSG_BAD_INPUT ""
+#include <string>
 
 #ifndef DEBUG
 # define DEBUG 0
 #endif // DEBUG
+
+# define MESSAGE_WELCOME "Hint: Please enter one of the following commands:" \
+						 " ADD, SEARCH, EXIT"
+# define MESSAGE_SEARCH "Hint: Please enter the index of the entry you want" \
+						" to be displayed"
+# define MESSAGE_SEARCH_BAD "Hint: The index must be between 0 - 8"
+void	debug_log(std::string message);
+void	debug_log(std::string message, int exit_status);
 
 #endif // PHONEBOOK_H
 
