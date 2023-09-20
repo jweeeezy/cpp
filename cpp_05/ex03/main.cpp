7403aa001540a711351d822eab8c944576e29910
jweeeezy <jwillert@42heilbronn.student.de>
2023-09-20 13:23:01 +0200

    cpp05-ex03: added more tests in main

--- cpp_05/ex03/main.cpp
+++ cpp_05/ex03/main.cpp
@@ -8,25 +8,62 @@
 //                                                                            //
 // -------------------------------------------------------------------------- //
 
-#include <iostream>   // needed for std::cout, std::endl
-#include <cstdlib>    // needed for MACROS
-#include "Intern.hpp" // needed for Intern class;
+#include <iostream>       // needed for std::cout, std::endl
+#include <cstdlib>        // needed for MACROS
+#include "Intern.hpp"     // needed for Intern class, AForm class,
+						  // Bureaucrat class
+
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
+static inline void print_story(std::string story)
+{
+	std::cout << GREEN << "\n< " << story << " >\n" << RESET << std::endl;
+}
 
 int	main(void)
 {
 	Intern i;
+	Bureaucrat b("Randy", 2);
 	
+	print_story("unknown form test");
 	try
 	{
-		AForm*f1 = i.makeForm("random form", "home");
-		AForm*f2 = i.makeForm("shrubbery creation", "home");
-
-		delete f1;
-		delete f2;
+		AForm* f = i.makeForm("random form", "home");
+		b.executeForm(*f);
+		delete f;
 	}
 	catch (std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
+	}
+
+	print_story("known form test");
+	try
+	{
+		AForm* f = i.makeForm("robotomy request", "fridge");
+		b.signForm(*f);
+		b.executeForm(*f);
+		delete f;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	print_story("NULL test / no try/catch block");
+	{
+		AForm* ptr = i.makeForm("not existing", "self");
+		// possible segfault if no exception thrown!
+		
+		if (ptr != NULL)
+		{
+			b.signForm(*ptr);
+		}
+		else
+		{
+			std::cout << "ptr is NULL!" << std::endl;
+		}
 	}
 
 	return (EXIT_SUCCESS);
