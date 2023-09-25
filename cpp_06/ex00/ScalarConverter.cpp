049294d84a51ca958d201f512de5b3abb8663c97
jweeeezy <jwillert@42heilbronn.student.de>
2023-09-25 19:28:44 +0200

    cpp06-ex00: added some documenation

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -9,12 +9,13 @@
 // -------------------------------------------------------------------------- //
 
 #include "ScalarConverter.hpp" // needed for ScalarConverter class
-
 #include <iostream> // needed for std::cout, std::endl, std::cerr
 
 #ifndef DEBUG
 # define DEBUG 0
 #endif // DEBUG
+
+/* <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> non-class functions */
 
 static inline void print_log(std::string message)
 {
@@ -24,16 +25,27 @@ static inline void print_log(std::string message)
 	}
 }
 
+/* <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> constructors */
+
+ScalarConverter::ScalarConverter(){;}
+ScalarConverter::ScalarConverter(const ScalarConverter& src){(void) src;}
+ScalarConverter::~ScalarConverter(){;}
+
+/* <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> operator overloads */
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& rhs)
+{
+	(void) rhs;
+	return *this;
+}
+
+/* <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> member functions */
+
 void ScalarConverter::convert(std::string input)
 {
 	print_log("convert() called!");
 	(void) input;
 	return ;
 }
-
-ScalarConverter::ScalarConverter(){;}
-ScalarConverter::ScalarConverter(const ScalarConverter& src){(void) src;}
-ScalarConverter::~ScalarConverter(){;}
-ScalarConverter& ScalarConverter::operator=(const ScalarConverter& rhs){(void) rhs; return *this;}
 
 // -------------------------------------------------------------------------- //
