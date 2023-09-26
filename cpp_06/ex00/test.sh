ece40e21624ea3fb7065c3e10087b3f3d04486c8
jweeeezy <jwillert@42heilbronn.student.de>
2023-09-26 23:13:02 +0200

    cpp06-ex00: made small tester. can add more cases now

--- cpp_06/ex00/test.sh
+++ cpp_06/ex00/test.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-
 # variables
 PROGRAM="./conversionofscalartypes"
 
-OUTPUT=$($PROGRAM asdfg)
-
-EXPECTED={"Char!", "Int!"}
+TESTS=(
+	"Char Test" "asdf." "Char!"
+	"Int Test" "12345" "Int!"
+	"Float Test" "23.01f" "Float!"
+	"Double Test" "23.045" "Double!"
+)
 
 echo
-for TEST_CASE in $EXPECTED; do
-	if [ "$OUTPUT" == "$TEST_CASE" ]; then
-		echo "$OUTPUT ✅"
+
+for ((i=0; i<${#TESTS[@]}; i+=3)); do
+	echo
+	test_name="${TESTS[$i]}"
+	test_value="${TESTS[$i+1]}"
+	expected_output="${TESTS[$i+2]}"
+	
+	actual_output="$PROGRAM $test_value"
+	
+	if [ "$($actual_output)" == "$expected_output" ] ; then
+		result=✅
 	else
-		echo "$OUTPUT ❌"
+		result=❌
 	fi
+
+	echo $test_name
+	echo $test_value $($actual_output) $expected_output $result
+	echo
 done
+
 echo
