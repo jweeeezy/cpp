221865f4b0000f3cd239e4762a3d37d6cdf28043
jweeeezy <willertjakob@gmail.com>
2023-11-13 06:20:25 +0100

    cpp_07-ex01: Finishes exercise

---
+++ cpp_07/ex01/iter.hpp
@@ -0,0 +1,31 @@
+// ========================================================================== //
+//                               42 | Heilbronn                               //
+// ========================================================================== //
+//                                                                            //
+// name:  jakob willert (jwillert)                                            //
+// mail:  jwillert@student.42heilbronn.de                                     //
+// file:  iter.hpp                                                            //
+//                                                                            //
+// -------------------------------------------------------------------------- //
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstdlib>  // needed for size_t
+#include <iostream> // needed for std::cout
+
+template <typename T, typename Func> void iter(T & arr, size_t length, Func f)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        f(arr[i]);
+    }
+};
+
+template <typename T> void print(const T & elem) { std::cout << elem << " "; }
+
+template <typename T> void multiplicate_by_2(T & elem) { elem *= 2; }
+
+#endif // ITER_HPP
+
+// -------------------------------------------------------------------------- //
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -8,18 +8,35 @@
 //                                                                            //
 // -------------------------------------------------------------------------- //
 
-#include <cstdlib> // needed for MACROS
+#include "iter.hpp" // needed for iter()
+#include <cstdlib>  // needed for MACROS
+#include <iostream> // needed for std::cout
+#include <string>   // needed for std::string
 
-#define EXPECTED_ARGC 2
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
 
-int	main(int argc, char **argv)
+static inline void test_log(std::string const & message)
 {
-	if (argc != EXPECTED_ARGC)
-	{
-		return (EXIT_FAILURE);
-	}
-	(void) argv;
-	return (EXIT_SUCCESS);
+    std::cout << GREEN << "\n< " << message << " >" << RESET << "\n";
+}
+
+int main(void)
+{
+    int c_arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    test_log("Printing with ::iter function");
+    ::iter(c_arr, 10, ::print<int>);
+    std::cout << "\n";
+
+    test_log("Multiplicating by 2 with ::iter function");
+    ::iter(c_arr, 10, ::multiplicate_by_2<int>);
+
+    test_log("Printin with ::iter function");
+    ::iter(c_arr, 10, ::print<int>);
+    std::cout << "\n\n";
+
+    return (EXIT_SUCCESS);
 }
 
 // -------------------------------------------------------------------------- //
