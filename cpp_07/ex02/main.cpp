b508cf92fc0ef251109dbd5a1503518e0a14af23
jweeeezy <willertjakob@gmail.com>
2023-11-15 20:16:46 +0100

    cpp07-ex02: Adds another test case for transparency

--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -24,6 +24,9 @@ static inline void test_log(std::string const & message)
 
 int main(void)
 {
+    test_log("Testing default constructor");
+    Array<Class> def;
+
     test_log("Testing size constructor");
     Array<Class> a(3);
     
@@ -34,15 +37,22 @@ int main(void)
     Array<Class> c = a;
 
     test_log("Testing sizes of all instances");
-    std::cout << "A size: " << a.size() << "\n";
-    std::cout << "B size: " << b.size() << "\n";
-    std::cout << "C size: " << c.size() << "\n";
+    std::cout << "def size: " << def.size() << "\n";
+    std::cout << "A size:   " << a.size() << "\n";
+    std::cout << "B size:   " << b.size() << "\n";
+    std::cout << "C size:   " << c.size() << "\n";
 
     test_log("Testing content of all instances");
     std::cout << "a[0]: " << a[0].content << "\n";
     std::cout << "b[1]: " << b[1].content << "\n";
     std::cout << "c[2]: " << c[2].content << "\n";
     
+    test_log("Testing if editing content changes copies");
+    a[1].content = "blub";
+    std::cout << "a[1]: " << a[1].content << "\n";
+    std::cout << "b[1]: " << b[1].content << "\n";
+    std::cout << "c[1]: " << c[1].content << "\n";
+
     test_log("Testing accessing out of bounds");
     try
     {
