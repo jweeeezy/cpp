634dcc8f96f4d18367f3f90a1a1ae367c219542a
jweeeezy <jwillert@42heilbronn.student.de>
2023-11-19 21:51:16 +0100

    cpp_08/ex02: finishes exercise

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -11,21 +11,64 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <stack> // needed for std::stack
+#include <iostream> // needed for std::cout
+#include <stack>    // needed for std::stack
+
+#define YELLOW "\033[33m"
+#define RESET  "\033[0m"
+
+/* <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> non-class functions */
+
+static inline void debug_log(std::string const & message)
+{
+    (void)message;
+#ifdef DEBUG
+    std::cout << YELLOW << "MutantStack: " << message << RESET << std::endl;
+#endif // DEBUG
+}
+
+/* <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> class */
 
 template <typename T> class MutantStack : public std::stack<T>
 {
+    
+    /* @note inheriting from std:: containers is not advised 
+     * (non virtual destructors) */
+
   public:
-    MutantStack(){};
-    MutantStack(MutantStack const & src) { (void)src; };
-    ~MutantStack(){};
+    /* constructors */
+    MutantStack()
+    {
+        debug_log("default constructor called\n");
+    }
+    MutantStack(MutantStack const & src) : std::stack<T>(src)
+    {
+        debug_log("copy constructor called\n");
+    }
+    ~MutantStack() 
+    {
+        debug_log("destructor called\n");
+    }
     MutantStack & operator=(MutantStack const & rhs)
     {
-        (void)rhs;
+        debug_log("assignment operator called\n");
+        if (this != &rhs)
+        {
+            this->c = rhs.c;
+        }
         return *this;
     };
+    
+    /* iterators */
+    typedef typename std::stack<T>::container_type   t_stack_ct;;
+    typedef typename t_stack_ct::iterator            iterator;
+    typedef typename t_stack_ct::const_iterator      const_iterator;
+    
+    iterator       begin()       { return this->c.begin(); }
+    iterator       end()         { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end()   const { return this->c.end(); }
 
-  private:
 };
 
 #endif // MUTANTSTACK_HPP
--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -8,13 +8,67 @@
 //                                                                            //
 // -------------------------------------------------------------------------- //
 
-#include <cstdlib> // needed for MACROS
-#include "MutantStack.hpp"
+#include "MutantStack.hpp" // needed for MutantStack class
+#include <cstdlib>         // needed for MACROS
+#include <list>            // needed for std::list
 
-int	main(void)
+void test_list()
 {
-    MutantStack<int> ms;
-	return (EXIT_SUCCESS);
+    std::list<int> lst;
+    lst.push_back(5);
+    lst.push_back(17);
+    std::cout << lst.back() << std::endl;
+    lst.pop_back();
+    std::cout << lst.back() << std::endl;
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    //[...]
+    lst.push_back(0);
+    std::list<int>::iterator it  = lst.begin();
+    std::list<int>::iterator ite = lst.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::list<int> l(lst);
+}
+
+void test_ms()
+{
+
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it  = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+}
+
+int main(void)
+{
+    test_ms();
+    std::cout << "\n";
+    test_list();
+    return (EXIT_SUCCESS);
 }
 
 // -------------------------------------------------------------------------- //
