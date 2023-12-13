288d940a271166c5b6b0d5709cc03f5a1515df7b
jweeeezy <willertjakob@gmail.com>
2023-12-13 22:42:15 +0100

    cpp09/ex02: finishes basic parsing (only positive integers)

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -8,6 +8,7 @@
 //                                                                            //
 // -------------------------------------------------------------------------- //
 
+#include <cstdlib>  // needed for std::atoi
 #include <iostream> // needed for std::cerr
 
 #include "PmergeMe.hpp" // needed for PmergeMe class
@@ -48,14 +49,62 @@ PmergeMe & PmergeMe::operator=(PmergeMe const & rhs)
     return *this;
 }
 
-void PmergeMe::parse_arguments(char ** argv)
+bool is_positive_number(t_str_c & input)
 {
+    size_t pos = input.find_first_not_of("0123456789+");
+    if (pos != t_str::npos || std::atoi(input.c_str()) <= 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+t_vec_str_c PmergeMe::str_split_by(t_str_c & str, t_str_c & delimiter)
+{
+    t_vec_str vec;
+    t_str     tmp(str);
+    while (1)
+    {
+        size_t pos = tmp.find_first_of(delimiter);
+        if (pos == t_str::npos)
+        {
+            pos = tmp.size();
+        }
+        vec.push_back(tmp.substr(0, pos));
+        if (pos == tmp.size())
+        {
+            break;
+        }
+        else
+        {
+            tmp = tmp.substr(pos + 1, tmp.size());
+        }
+    }
+    return vec;
+}
+
+t_vec_str_c PmergeMe::split_quotated_strings(char ** argv)
+{
+    t_vec_str vec;
     for (int i = 1; i <= _argc; ++i)
     {
-        int tmp = std::atoi(argv[i]);
-        std::cout << tmp << " ";
+        t_vec_str_c tmp = str_split_by(argv[i], " ");
+        vec.insert(vec.end(), tmp.begin(), tmp.end());
     }
-    std::cout << "\n";
+    return vec;
+}
+
+t_vec_str_c PmergeMe::parse_arguments(char ** argv)
+{
+    t_vec_str_c vec = split_quotated_strings(argv);
+    for (t_vec_str_cit it = vec.begin(); it != vec.end(); ++it)
+    {
+        if (is_positive_number(*it) == false)
+        {
+            throw std::invalid_argument("only positive integers allowed!");
+        }
+    }
+    return vec;
 }
 
 // -------------------------------------------------------------------------- //
--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -12,9 +12,19 @@
 #define PMERGEME_HPP
 
 #include <string> // needed for std::string
+#include <vector> // needed for std::vector
+
+/* @note typedefs */
+typedef std::string t_str;
+typedef t_str const t_str_c;
+
+typedef std::vector<t_str>        t_vec_str;
+typedef t_vec_str const           t_vec_str_c;
+typedef t_vec_str::const_iterator t_vec_str_cit;
 
 class PmergeMe
 {
+
   public:
     PmergeMe(int argc, char ** argv);
     PmergeMe();
@@ -22,13 +32,16 @@ class PmergeMe
     ~PmergeMe();
     PmergeMe & operator=(PmergeMe const & rhs);
 
-    void log_debug(std::string const & message) const;
+    void log_debug(t_str_c & message) const;
 
-    void parse_arguments(char ** argv);
+    t_vec_str_c str_split_by(t_str_c & str, t_str_c & delimiter);
+    t_vec_str_c split_quotated_strings(char ** argv);
+    t_vec_str_c parse_arguments(char ** argv);
 
+  private:
     int _argc;
     /* @note prob a template Container ? */
-        /* or just an overloaded constructor */
+    /* or just an overloaded constructor */
 };
 
 #endif // PMERGEME
