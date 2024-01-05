36386439d1cce1d67458268f8e33890dfdaa47ef
jweeeezy <jwillert@42heilbronn.student.de>
2024-01-05 13:41:47 +0100

    cpp09/ex02: adds t_pair as typedef

--- cpp_09/ex02/typedefs.hpp
+++ cpp_09/ex02/typedefs.hpp
@@ -11,8 +11,8 @@
 #ifndef TYPEDEFS_HPP
 #define TYPEDEFS_HPP
 
-#define RED        "\033[1;31m"
-#define RESET      "\033[0m"
+#define RED   "\033[1;31m"
+#define RESET "\033[0m"
 
 /* only needed for debugging */
 #ifdef DEBUG
@@ -27,33 +27,36 @@
 #include <string> // needed for std::string
 #include <vector> // needed for std::vector
 
-typedef std::string t_str;
-typedef t_str const t_str_c;
+typedef std::string                    t_str;
+typedef t_str const                    t_str_c;
 
-typedef std::vector<t_str>        t_vec_str;
-typedef t_vec_str const           t_vec_str_c;
-typedef t_vec_str::iterator       t_vec_str_it;
-typedef t_vec_str::const_iterator t_vec_str_cit;
+typedef std::pair<int, int>            t_pair;
+typedef t_pair const                   t_pair_c;
 
-typedef std::list<int>            t_lst_int;
-typedef t_lst_int const           t_lst_int_c;
-typedef t_lst_int::iterator       t_lst_int_it;
-typedef t_lst_int::const_iterator t_lst_int_cit;
+typedef std::vector<t_str>             t_vec_str;
+typedef t_vec_str const                t_vec_str_c;
+typedef t_vec_str::iterator            t_vec_str_it;
+typedef t_vec_str::const_iterator      t_vec_str_cit;
 
-typedef std::list<std::pair<int, int> > t_lst_pair_int;
-typedef t_lst_pair_int const            t_lst_pair_int_c;
-typedef t_lst_pair_int::iterator        t_lst_pair_int_it;
-typedef t_lst_pair_int::const_iterator  t_lst_pair_int_cit;
+typedef std::list<int>                 t_lst_int;
+typedef t_lst_int const                t_lst_int_c;
+typedef t_lst_int::iterator            t_lst_int_it;
+typedef t_lst_int::const_iterator      t_lst_int_cit;
 
-typedef std::deque<int>           t_deq_int;
-typedef t_deq_int const           t_deq_int_c;
-typedef t_deq_int::iterator       t_deq_int_it;
-typedef t_deq_int::const_iterator t_deq_int_cit;
+typedef std::list<t_pair>              t_lst_pair_int;
+typedef t_lst_pair_int const           t_lst_pair_int_c;
+typedef t_lst_pair_int::iterator       t_lst_pair_int_it;
+typedef t_lst_pair_int::const_iterator t_lst_pair_int_cit;
 
-typedef std::deque<std::pair<int, int> > t_deq_pair_int;
-typedef t_deq_pair_int const             t_deq_pair_int_c;
-typedef t_deq_pair_int::iterator         t_deq_pair_int_it;
-typedef t_deq_pair_int::const_iterator   t_deq_pair_int_cit;
+typedef std::deque<int>                t_deq_int;
+typedef t_deq_int const                t_deq_int_c;
+typedef t_deq_int::iterator            t_deq_int_it;
+typedef t_deq_int::const_iterator      t_deq_int_cit;
+
+typedef std::deque<t_pair>             t_deq_pair_int;
+typedef t_deq_pair_int const           t_deq_pair_int_c;
+typedef t_deq_pair_int::iterator       t_deq_pair_int_it;
+typedef t_deq_pair_int::const_iterator t_deq_pair_int_cit;
 
 #endif // TYPEDEFS_HPP
 
